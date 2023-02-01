9c9f23e148c1852fd8a7b34e87c8e30250c2b398
jhjang <jhjang@unet.kr>
2023-02-01 17:29:34 +0900

    commit

--- Algo_stud_space/rest/src/main.rs
+++ Algo_stud_space/rest/src/main.rs
@@ -11,14 +11,20 @@ fn main() {
 
 //    let a : i32 = get_value();
     println!("write value a, b, c. separate each as Enter");
-    let mut inputString = String::new();
-    io::stdin().read_line(&mut inputString).expect("Wrong input");
-    let mut inputValue : Vec<&str> = inputString.split(' ').collect();
+    let mut inputString = String::new(); // 3개의 변수(a,b,c)를 하나의 줄에 다 입력받는다.
+    io::stdin().read_line(&mut inputString).expect("Wrong input"); // 1줄로 입력받음
+    let mut inputValue : Vec<&str> = inputString.split(' ').collect(); // std::io::stdin 의 split
+                                                                       // 을 이용해서 공백을
+                                                                       // 기준으로 나누고 collect
+                                                                       // 로 벡터에 차례차례
+                                                                       // 수집함.
 
-    let mut a = &inputValue[0];
-    let mut b = &inputValue[1];
-    let mut c = &inputValue[2];
+    let mut a = &inputValue[0]; // 처음 받는 숫자는 a
+    let mut b = &inputValue[1]; // 두번째 받는 숫자는 b
+    let mut c = &inputValue[2]; // 세번째 받는 숫자는 c 가 된다.
 
+// split 으로 나눈 벡터안의 값들이 String 인 채 이므로 u32 로 변환하는 함수를 통해 u32로 바꾼다.
+// 아래에 fn convert_u32 로 구현해두었음.
     let mut a = convert_u32(&a);
     let mut b = convert_u32(&b);
     let mut c = convert_u32(&c);
@@ -48,11 +54,16 @@ fn get_value() -> String {
     std::io::stdin().read_line(&mut buffer).expect("Failed");
     buffer
 }
-
+// 전달받는 값이 스트링슬라이스 (&str) 이므로 변수 타입 지정. u32 값을 원하므로 output 은 u32 가
+// 되게 선언
 fn convert_u32 (input_value : &str ) -> u32 {
+    //trim 메소드는 처음과 끝 부분의 빈칸을 제거한다. parse 메소드는 u32 로 변환해준다.
     let a : u32 = match input_value.trim().parse(){
+        // 문제없을 경우 a로 그대로 반환
         Ok(a) => a,
+        // 문제가 있을 경우 panic 을 일으키고 변환중에 일으켰으며, 나온 에러값을 출력하도록 함.
         Err(error) => { panic!("There was a problem converting the value {:?}",error)},
     };
+    // 이제 변환된 값을 함수의 output 으로 보냄.
     a
 }
