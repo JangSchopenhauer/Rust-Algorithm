210c27f287cd021e3f17ecf6dd07defd875db87e
JangSchopenhauer <jhjang@unet.kr>
2023-02-02 15:10:16 +0900

    Self Number Code added. it is not completed, need to rebuild_v0.1

--- Algo_stud_space/SelfNumber_4673/src/main.rs
+++ Algo_stud_space/SelfNumber_4673/src/main.rs
@@ -1,22 +1,20 @@
 fn main() {
-
-    let mut a = 1;
-    println!("{:?}",a);
-    for a in 1..10 {
-        let mut a = a + 2;
-        println!("{:?}",a);
-    };
-    for a in 10..100 {
-        let mut a = a + 11;
-        println!("{:?}",a);
-    };
-    for a in 100..1000 {
-        let mut a = a+101;
-        println!("{:?}",a);
-    };
-    for a in 1000..10000 {
-        let mut a = a+1001;
-        println!("{:?}",a);
-    };
+    let mut numbers = [true; 10001];
+    for i in 1..10000 {
+        let mut n = i;
+        let mut m = i;
+        while n > 0 {
+            m += n % 10;
+            n /= 10;
+        }
+        if m <= 10000 {
+            numbers[m as usize] = false;
+        }
+    }
+    for i in 1..10000 {
+        if numbers[i as usize] {
+            println!("{} is a self number", i);
+        }
+    }
 }
 
